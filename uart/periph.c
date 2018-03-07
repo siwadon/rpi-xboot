4359039ade25e7eb8c305004e5c723818d8196e1
Siwadon Saosoong <siwadon.s@gmail.com>
2018-03-06 20:21:20 -0800

    Refactor: clean up uart functions
    - remove unused functions
    - extract UART line status checking functions

--- uart/periph.c
+++ uart/periph.c
@@ -5,8 +5,7 @@
 
 extern void mmio_write(unsigned int, unsigned int);
 extern unsigned int mmio_read(unsigned int);
-extern void dummy(unsigned int);
-
+extern void delay(unsigned int);
 /**
  * BCM2835 ARM Peripherals
  * https://www.raspberrypi.org/app/uploads/2012/02/BCM2835-ARM-Peripherals.pdf
@@ -40,47 +39,60 @@ extern void dummy(unsigned int);
 #define AUX_MU_STAT_REG (UART_BASE + 0x64)  // Mini UART Extra Status
 #define AUX_MU_BAUD_REG (UART_BASE + 0x68)  // Mini UART Baudrate
 
-//GPIO14  TXD0 and TXD1
-//GPIO15  RXD0 and RXD1
-unsigned int uart_lcr(void)
+void uart_init(void)
 {
-    return (mmio_read(AUX_MU_LSR_REG));
+    unsigned int ra;
+
+    mmio_write(AUX_ENABLES,     1);
+    mmio_write(AUX_MU_IER_REG,  0);
+    mmio_write(AUX_MU_CNTL_REG, 0);
+    mmio_write(AUX_MU_LCR_REG,  3);
+    mmio_write(AUX_MU_MCR_REG,  0);
+    mmio_write(AUX_MU_IER_REG,  0);
+    mmio_write(AUX_MU_IIR_REG,  0xC6);
+    mmio_write(AUX_MU_BAUD_REG, 270);
+
+    // GPIO 14 : TXD0 and TXD1
+    // GPIO 15 : RXD0 and RXD1
+    ra = mmio_read(GPFSEL1);
+    ra &= ~(7 << 12); // GPIO 14
+    ra |= 2 << 12;    // ALT 5
+    ra &= ~(7 << 15); // GPIO 15
+    ra |= 2 << 15;    // ALT 5
+
+    mmio_write(GPFSEL1, ra);
+    mmio_write(GPPUD, 0);
+    delay(150);
+
+    mmio_write(GPPUDCLK0, (1 << 14) | (1 << 15));
+    delay(150);
+
+    mmio_write(GPPUDCLK0, 0);
+    mmio_write(AUX_MU_CNTL_REG, 3);
 }
 
-unsigned int uart_recv(void)
-{
-    while (1)
-    {
-        if (mmio_read(AUX_MU_LSR_REG) & 0x01)
-            break;
-    }
-    return (mmio_read(AUX_MU_IO_REG) & 0xFF);
+int is_uart_data_ready() {
+    return mmio_read(AUX_MU_LSR_REG) & 0x01;
 }
 
-unsigned int uart_check(void)
-{
-    if (mmio_read(AUX_MU_LSR_REG) & 0x01)
-        return (1);
-    return (0);
+int is_uart_transmitter_ready() {
+    return mmio_read(AUX_MU_LSR_REG) & 0x20;
 }
 
-void uart_send(unsigned int c)
+char uart_getc(void)
 {
-    while (1)
-    {
-        if (mmio_read(AUX_MU_LSR_REG) & 0x20)
-            break;
-    }
+    // Wait for UART to have received something
+    while (!is_uart_data_ready()) { }
+
+    return mmio_read(AUX_MU_IO_REG) & 0xFF;
+}
+
+void uart_putc(unsigned int c)
+{
+    // Wait for UART to become ready to transmit
+    while (!is_uart_transmitter_ready()) { }
+
     mmio_write(AUX_MU_IO_REG, c);
-}
-
-void uart_flush(void)
-{
-    while (1)
-    {
-        if ((mmio_read(AUX_MU_LSR_REG) & 0x100) == 0)
-            break;
-    }
 }
 
 void hexstrings(unsigned int d)
@@ -93,61 +105,37 @@ void hexstrings(unsigned int d)
     {
         rb -= 4;
         rc = (d >> rb) & 0xF;
+
         if (rc > 9)
             rc += 0x37;
         else
             rc += 0x30;
-        uart_send(rc);
+            
+        uart_putc(rc);
+
         if (rb == 0)
             break;
     }
-    uart_send(0x20);
+
+    uart_putc(0x20);
 }
 
 void hexstring(unsigned int d)
 {
     hexstrings(d);
-    uart_send(0x0D);
-    uart_send(0x0A);
-}
-
-void uart_init(void)
-{
-    unsigned int ra;
-
-    mmio_write(AUX_ENABLES, 1);
-    mmio_write(AUX_MU_IER_REG, 0);
-    mmio_write(AUX_MU_CNTL_REG, 0);
-    mmio_write(AUX_MU_LCR_REG, 3);
-    mmio_write(AUX_MU_MCR_REG, 0);
-    mmio_write(AUX_MU_IER_REG, 0);
-    mmio_write(AUX_MU_IIR_REG, 0xC6);
-    mmio_write(AUX_MU_BAUD_REG, 270);
-    ra = mmio_read(GPFSEL1);
-    ra &= ~(7 << 12); //gpio14
-    ra |= 2 << 12;    //alt5
-    ra &= ~(7 << 15); //gpio15
-    ra |= 2 << 15;    //alt5
-    mmio_write(GPFSEL1, ra);
-    mmio_write(GPPUD, 0);
-    for (ra = 0; ra < 150; ra++)
-        dummy(ra);
-    mmio_write(GPPUDCLK0, (1 << 14) | (1 << 15));
-    for (ra = 0; ra < 150; ra++)
-        dummy(ra);
-    mmio_write(GPPUDCLK0, 0);
-    mmio_write(AUX_MU_CNTL_REG, 3);
+    uart_putc(0x0D); // carriage return
+    uart_putc(0x0A); // new line
 }
 
 void timer_init(void)
 {
-    //0xF9+1 = 250
-    //250MHz/250 = 1MHz
+    // 0xF9 + 1 = 250
+    // 250 MHz/250 = 1 MHz
     mmio_write(ARM_TIMER_CTL, 0x00F90000);
     mmio_write(ARM_TIMER_CTL, 0x00F90200);
 }
 
 unsigned int timer_tick(void)
 {
-    return (mmio_read(ARM_TIMER_CNT));
+    return mmio_read(ARM_TIMER_CNT);
 }
--- uart/start.s
+++ uart/start.s
@@ -1,21 +1,26 @@
 .globl _start
 _start:
-    mov sp,#0x8000
+    mov sp, #0x8000
     bl kernel_main
 hang: b hang
 
+@ Memory-Mapped I/O output
 .globl mmio_write
 mmio_write:
-    str r1,[r0]
+    str r1, [r0]
     bx lr
 
+@ Memory-Mapped I/O input
 .globl mmio_read
 mmio_read:
-    ldr r0,[r0]
+    ldr r0, [r0]
     bx lr
 
-.globl dummy
-dummy:
+@ Loop delay times
+.globl delay
+delay:
+    subs r0, r0, #1
+    bne delay
     bx lr
 
 .globl GETPC
