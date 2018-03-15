bd09c8edfa372acfe3efb973e4779841760bfb06
Siwadon Saosoong <siwadon.s@gmail.com>
2018-03-14 17:06:57 -0700

    Keep printing dot

--- uart.c
+++ uart.c
@@ -1,6 +1,7 @@
 extern void mmio_write(unsigned int, unsigned int);
 extern unsigned int mmio_read(unsigned int);
 extern unsigned int get_pc(void);
+extern void delay(unsigned int);
 
 extern void uart_init(void);
 extern void uart_putc(unsigned int);
@@ -16,6 +17,11 @@ int kernel_main(void)
     uart_putx(0x12345678);
     uart_putx(0xABCDEF);
     uart_putx(get_pc());
+
+    while(1) {
+        delay(10000000);
+        uart_puts(".");
+    }
 
     return 0;
 }
