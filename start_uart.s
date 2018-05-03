64cb26d6ad213044312e4dc77c7a31ddbe65d226
Siwadon Saosoong <siwadon.s@gmail.com>
2018-05-03 12:18:09 -0700

    Add LED blinker to uart program

--- start_uart.s
+++ start_uart.s
@@ -1,5 +1,4 @@
 .globl _start
-
 _start:
     b loader_start
     b loader_sleep  // undefined
@@ -26,7 +25,7 @@ loader_start:
     ands r0, r0, #0x03
     bne loader_sleep
 
-    mov sp, #0x10000
+    mov sp, #0x08000000
     bl kernel_main
 
 hang:
@@ -65,9 +64,76 @@ delay:
 
 .globl get_pc
 get_pc:
-    mov r0,lr
+    mov r0, lr
     bx lr
 
 .globl branch_to
 branch_to:
     bx r0
+
+.global mailbox_write
+mailbox_write:
+    ldr         r1, =0x3f00b880             @; load the hex number =0x3f00b880 into register r1
+                                            @; this is the base address of the mailboxes
+    wait$:
+        ldr     r2, [r1, #0x38]             @; load r2 with the address of the status for mailbox 1 (ARM -> VC)
+        tst     r2, #0x80000000             @; check if the FIFO queue is full
+        bne     wait$                       @; branch to wait$ label if the queue is full
+
+    str         r0, [r1, #0x20]             @; put the message into mailbox 1 write register, which is at offset 0x20 from the base address
+    mov         pc, lr                      @; return
+
+.global mailbox_read
+mailbox_read:
+    push        {lr}                        @; push the address the function should return to
+    mov         r1, r0                      @; save the channel in r1
+    ldr         r0, =0x3f00b880             @; load mailbox base address into r0
+
+    right_mail$:
+        wait_read$:
+            ldr     r2, [r0, #0x18]         @; get status of mailbox 0 (VC -> ARM)
+            tst     r2, #0x40000000         @; check if the mailbox is empty
+            bne     wait_read$              @; if VC FIFO queue is empty branch to wait_read
+
+        ldr     r2, [r0]                    @; load the address of response data
+
+        and     r3, r2, #0b1111             @; extract the channel, the lowest 4 bits
+        teq     r3, r1                      @; check if the response channel is the same we want
+        bne     right_mail$                 @; if the channel is wrong branch to wait_read
+
+    and         r0, r2, #0xfffffff0         @; move the response (top 28 bits of mail) into r0
+    pop         {pc}
+
+.global set_led_state
+set_led_state:
+    push        {lr}                @; save address the function should return to
+    mov         r1, r0              @; move the led state to r1
+    ldr         r0, =mail_message   @; load the message into r0
+    mov         r2, #0
+    str         r2, [r0, #4]        @; reset request code
+    str         r2, [r0, #16]       @; reset request/response size
+    mov         r2, #130
+    str         r2, [r0, #20]       @; reset pin number
+
+    str         r1, [r0, #24]       @; overwrite the led state
+    add         r0, #8              @; add the channel 8 as the last 4 bits of the message
+    bl          mailbox_write       @; write the message to mailbox
+    mov         r0, #8              @; the channel to read the message from
+    bl          mailbox_read        @; read the message to prevent the FIFO queue to get full
+    pop         {pc}                @; return
+
+.section .data
+.align 4                            @; last 4 bits of the next label set to 0 (16-byte alligned)
+mail_message:
+    .int    size                    @; #0 message header contains the size of the message
+    .int    0                       @; #4 request code 0
+
+    .int    0x00038041              @; #8 header tag ID
+    .int    8                       @; #12 size of tag data
+    .int    0                       @; #16 request/response size
+
+    .int    130                     @; #20 pin number
+    .int    1                       @; #24 pin state
+    .int    0                       @; #28 signal the GPU that the message is over
+size:
+    .int    . - mail_message        @; size of the message
--- uart.c
+++ uart.c
@@ -2,6 +2,7 @@ extern void mmio_write(unsigned int, unsigned int);
 extern unsigned int mmio_read(unsigned int);
 extern unsigned int get_pc(void);
 extern void delay(unsigned int);
+extern void set_led_state(unsigned int);
 
 extern void uart_init(void);
 extern void uart_putc(unsigned int);
@@ -19,8 +20,11 @@ int kernel_main(void)
     uart_putx(get_pc());
 
     while(1) {
-        delay(10000000);
         uart_puts(".");
+        set_led_state(1);
+        delay(5000000);
+        set_led_state(0);
+        delay(5000000);
     }
 
     return 0;
